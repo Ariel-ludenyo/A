6bfcd75e0f3211f8579e47dcdfe99b0e87781dcd
Ariel-ludenyo <124559795+Ariel-ludenyo@users.noreply.github.com>
2023-05-30 18:43:36 +0300

    Add files via upload

---
+++ QUIZ GAME.py
@@ -0,0 +1,47 @@
+from ast import If
+from cgi import print_directory
+
+
+print_directory ("welcome to my computer quiz game") # type: ignore # 
+
+Playing  = input ("Do you want to play? ")
+
+If
+
+playing != "yes":  # type: ignore
+quit ()
+print ("okay let's play") 
+
+answer= input("What does GUI stands for?")
+if answer == ("graphics user interface. ")
+          Print "correct": # type: ignore
+else:
+        print("Incorect")
+ answer = input ('what does AI stands for? '
+if 
+answer= "artificial inteligence"
+         print ("correct")
+"else"
+        print ('incorrect')                      
+
+nswer= input("what does RAM stands for?")
+if answer == "random accessing memory"
+          Print "correct"
+"else"
+        print("Incorect")
+ answer = input ("what does OS stands for? ")  
+if answer= "operating systems"
+         print ("correct")
+else :
+        print ('incorrect')     
+
+answer = input("What does CPU stands for?")
+if answer == "centrol processing unit"
+          Print "correct"
+else:
+        print("Incorect")
+ answer = input ("what does GPU stands for? ")  
+if answer= "graphics processing unit"
+         print ("correct")
+else :
+        print ('incorrect')     
